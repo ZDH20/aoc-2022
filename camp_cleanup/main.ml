9b45c253cca872432e8574f6444fda8a54e69c8f
= <=>
2023-05-10 22:46:18 -0500

    renaiming

--- camp_cleanup/main.ml
+++ camp_cleanup/main.ml
@@ -4,17 +4,15 @@ let read_whole_file filename =
   close_in ch;
   s
 
-let in_range (x: int) (floor: int) (ceiling: int): bool =
-  x >= floor && x < ceiling
-
-let convert_string_to_int (data: string): int * int =
+let str_to_i32 (data: string): int * int =
   match String.split_on_char '-' data with
   | [a; b] -> (int_of_string a, int_of_string b)
   | _ -> failwith "Could not convert to integer left"
 
 let part2 (left: string) (right: string): int =
-  let first = convert_string_to_int left in
-  let second = convert_string_to_int right in
+  let in_range k x y = k >= x && k < y in
+  let first = str_to_i32 left in
+  let second = str_to_i32 right in
   let f1, f2 = fst first, snd first in
   let s1, s2 = fst second, snd second in
   if (in_range f1 s1 (s2 + 1) || in_range f2 s1 (s2 + 1))
@@ -22,8 +20,8 @@ let part2 (left: string) (right: string): int =
   else 0
 
 let part1 (left: string) (right: string): int =
-  let first = convert_string_to_int left in
-  let second = convert_string_to_int right in
+  let first = str_to_i32 left in
+  let second = str_to_i32 right in
   let f1, f2 = fst first, snd first in
   let s1, s2 = fst second, snd second in
   if (f1 <= s1 && f2 >= s2) || (s1 <= f1 && s2 >= f2) then 1
@@ -40,14 +38,12 @@ let rec find_overlapping (lines: string list) (sum: int): int =
          | hd' :: tl' :: [] -> hd', tl'
          | _ -> failwith "Could not parse line."
        in
-       let x = part2 left right in
-       find_overlapping tl (sum + x)
+       find_overlapping tl (sum + (part2 left right))
 
 let filepath = "./input.txt"
 
 let () =
   let data = read_whole_file filepath in
   let lines = String.split_on_char '\n' data in
-  find_overlapping lines 0
-  |> Printf.printf "%d\n"
+  find_overlapping lines 0 |> Printf.printf "%d\n"
 
