b64d4bab743941c66c1236e328e1688727b81c88
Zachary Haskins <zdh@pop-os.localdomain>
2023-05-11 22:24:19 -0500

    part 1 of day 9

---
+++ rope_bridge/main.py
@@ -0,0 +1,55 @@
+#!/bin/python3
+
+import math
+
+def distance(x1, y1, x2, y2):
+    m1 = (x2 - x1) ** 2
+    m2 = (y2 - y1) ** 2
+    return math.sqrt(m1 + m2)
+
+
+def move(direction):
+    global head, tail, tail_history, head_history, visited
+
+    head_history.append([head[0], head[1]])
+    tail_history.append([tail[0], tail[1]])
+
+    if direction == 'R':
+        head[0] += 1
+    elif direction == 'L':
+        head[0] -= 1
+    elif direction == 'U':
+        head[1] += 1
+    else:
+        head[1] -= 1
+
+    dist = distance(head[0], head[1], tail[0], tail[1])
+
+    if dist >= 2:
+        tail[0], tail[1] = head_history[-1]
+
+    if (tail[0], tail[1]) not in visited:
+        visited.add((tail[0], tail[1]))
+
+
+file = open("input.txt")
+data = file.read()
+
+tail = [0, 0]
+head = [0, 0]
+
+tail_history = []
+head_history = []
+
+visited = set()
+
+for entry in data.split('\n'):
+    try:
+        direction, steps = entry.split()
+    except:
+        break
+    for _ in range(0, int(steps)):
+        move(direction)
+
+print(len(visited))
+
